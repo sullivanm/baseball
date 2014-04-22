5be5016e8703849fcdcf2a3e5056ac7f6440029f
sullivanm <sullivancoder@gmail.com>
2014-04-22 03:47:30 -0700

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baseball::Application.config.secret_key_base = '8050c6f67c3b12add456af25cea88fc0a59947de1d4a947dd992f59e7e2a80f35924dcde0d198361d21e5b030f5b7e9fbc9edea63b6b4f947d3343862db9451e'
